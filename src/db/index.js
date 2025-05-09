3791d6d991082e7eacf7b2bb26fcf6a6278afc72
HyperAfnan <afnanmalik2712@gmail.com>
2025-05-09 13:58:28 +0530

    fix: collection name was test

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,8 @@
 import mongoose from "mongoose";
-const clientOptions = {
-  serverApi: {version: "1", strict: true, deprecationErrors: true},
-};
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGODB_URI}`, clientOptions);
-    await mongoose.connection.db.admin().command({ping: 1});
+    await mongoose.connect(`${process.env.MONGODB_URI}/videotube` );
     console.log(`DB Connected `);
   } catch (e) {
     console.error(e);
