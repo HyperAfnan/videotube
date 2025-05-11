14e7905072a5eae84c98947020c77d229723d24e
HyperAfnan <afnanmalik2712@gmail.com>
2025-05-11 22:09:36 +0530

    added body-parser dependency

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -9,6 +10,7 @@ app.use(express.json({ limit: "32kb" }));
 app.use(express.urlencoded({ extended: true, limit: "32kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+app.use(bodyParser.json());
 
 import userRouter from "./routes/user.routes.js";
 import healthRouter from "./routes/health.routes.js";
