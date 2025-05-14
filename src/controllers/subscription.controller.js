7c69c94d1f207b719ebe0d07abccac62ccb8d8dd
HyperAfnan <afnanmalik2712@gmail.com>
2025-05-14 17:44:07 +0530

    feat(subscription): Implement subscription toggle functionality
    * Added logic to check if a user is already subscribed to a channel
    * Implemented subscription creation when a user subscribes
    * Added unsubscription capability when a user toggles an existing
      subscription
    * Included validation for channel ID to ensure proper data handling
    * Added appropriate success responses with status codes for both
      subscription and unsubscription actions

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -8,6 +8,48 @@ import asyncHandler from "../utils/asyncHandler.js";
 const toggleSubscription = asyncHandler(async (req, res) => {
 	const { channelId } = req.params;
 	// TODO: toggle subscription
+
+	// get channelId from params
+	// get userId from req.user._id
+	// validate both ids
+	// check if already subscribed, if subscribed
+	// delete existing subscription with both ids
+	//          or
+	// create new subscription with both ids
+	// return the subscription object
+
+	if (!channelId) {
+		throw new ApiError(404, "Channel not found");
+	}
+
+	const isValidChannel = isValidObjectId(channelId);
+	if (!isValidChannel) {
+		throw new ApiError(400, "Invalid Channel Id");
+	}
+
+	const isSubscribed = await Subscription.find({
+		channel: channelId,
+		subscriber: req.user._id,
+	});
+
+	var subscription;
+	if (isSubscribed.length > 0) {
+		subscription = await Subscription.deleteOne({
+			channel: channelId,
+			subscriber: req.user._id,
+		});
+		res
+			.status(200)
+			.json(new ApiResponse(200, subscription, "Successfully unsubscribed"));
+	} else {
+		subscription = await Subscription.create({
+			channel: channelId,
+			subscriber: req.user._id,
+		});
+		res
+			.status(200)
+			.json(new ApiResponse(200, subscription, "Successfully subscribed"));
+	}
 });
 
 // controller to return subscriber list of a channel
