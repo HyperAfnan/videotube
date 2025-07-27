ebb54f3ddf95cbd47106cb8620ff6f4a9e1d3a05
HyperAfnan <afnanmalik2712@gmail.com>
2025-07-28 02:31:34 +0530

    Made a frontend starter

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+         <div className="flex items-center justify-center bg-red-500">
+            hello world
+         </div>
+    </>
+  )
+}
+
+export default App
